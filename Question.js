c408faf6ce6ff6deacfcdb8905868e137b29c5b4
kingdavid198-netizen <kingdavid198@gmail.com>
2026-05-27 16:31:38 +0100

    Added Question model

---
+++ Question.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const QuestionSchema = new mongoose.Schema({
+    subject: String,
+    topic: String,
+    question: String,
+    optionA: String,
+    optionB: String,
+    optionC: String,
+    optionD: String,
+    answer: String,
+    image: String
+});
+
+module.exports = mongoose.model("Question", QuestionSchema);
