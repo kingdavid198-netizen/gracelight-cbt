96945d4460fce814aec90fc15972d83d0267ca35
kingdavid198-netizen <kingdavid198@gmail.com>
2026-05-29 01:46:12 +0100

    Update Result.js

--- Result.js
+++ Result.js
@@ -5,6 +5,7 @@ const ResultSchema = new mongoose.Schema({
     subject: String,
     topic: String,
     score: Number,
+    totalQuestions: Number
     percentage: Number,
     remark: String,
     date: String
